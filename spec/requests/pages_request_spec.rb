50ad0b89498d56050cfc668d2dc28bb80502ba18
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-01-21 10:34:59 +1030

    created root route and tests for homepage

---
+++ spec/requests/pages_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+    describe "GET /" do
+        before(:each) do 
+            get '/'
+        end 
+        it "should respond with a status of 200" do 
+            expect(response).to have_http_status(:success)
+        end
+        
+        it "should render the pages/index page" do 
+            expect(response).to render_template("index")
+        end 
+    end 
+end
