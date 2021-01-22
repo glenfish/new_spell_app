8ecf25416c7edf2a34644d2f8529a6fb72a0938e
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-01-22 13:31:46 +1030

    lunch time

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
-  get "/spells", to: "spells#index"
+  get "/spells", to: "spells#index", as:"spells"
   post "/spells", to: "spells#create"
-  get "/spells/:id", to: "spells#show"
+  get "/spells/:id", to: "spells#show", as: "spell"
   put "/spells/:id", to: "spells#update"
   delete "/spells/:id", to: "spells#destroy"
 
